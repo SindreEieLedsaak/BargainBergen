5ca486f07c25f4fa406062a206ea115cda71bc01
Jonatan Valen <102178908+jvalen01@users.noreply.github.com>
2024-02-23 11:43:45 +0100

    successfully connected to MongoDB

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Connection URI
-const uri = 'mongodb+srv://jvalen01:<StudentiBergen>@cluster251.3ybeuw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster251';
+const uri = 'mongodb+srv://jvalen:StudentiBergen@cluster251.2bmzlk3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster251';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
