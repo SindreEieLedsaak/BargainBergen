26453c4077c82ff67ba205434d144363c3c20e67
Jonatan Valen <102178908+jvalen01@users.noreply.github.com>
2024-02-27 10:29:48 +0100

    Created user model, controller and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,42 @@
+const User = require('../models/User');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+      
+
+const createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+  
+      if (!user) {
+        return res.status(404).send({ message: "User not found." });
+      }
+  
+      res.status(200).send({ message: "User deleted successfully." });
+    } catch (error) {
+      res.status(500).send({ message: "Error deleting user.", error: error });
+    }
+  };
+
+module.exports = {
+    getAllUsers,
+    createUser,
+    deleteUser,
+    // ... any other user-related functions ...
+  };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); // Adjust the path as necessary
-
-// POST: Create a new user
-router.post('/', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+const userController = require('../controllers/userController');
 
 // GET: Retrieve all users
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router.get('/', userController.getAllUsers);
 
+// POST: Create a new user
+router.post('/', userController.createUser);
 
+router.delete('/:id', userController.deleteUser); // Delete a specific user by id
 
 module.exports = router;
+
