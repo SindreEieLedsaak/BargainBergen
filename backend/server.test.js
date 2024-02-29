48f80c0f799b30b246e4967967ad5f6840b5d8cd
vegardvasset <vva034@student.uib.no>
2024-02-29 16:02:39 +0100

    Corrected writing

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,6 +1,6 @@
 // server.test.js
 const request = require('supertest');
-const app = require('./server'); // Adjust the path as necessary if your server.js file is in a different directory
+const app = require('./server'); 
 
 describe('API Endpoint Testing', () => {
   it('GET /api should respond with a message', async () => {
