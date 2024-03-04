65edc5b5af734f3c053cdb5355773c135c3499fe
vegardvasset <vva034@student.uib.no>
2024-03-04 11:58:45 +0100

    Cypress setup

---
+++ client/cypress/e2e/sample_test.cy.js
@@ -0,0 +1,11 @@
+
+// Test that the home page loads successfully
+describe('The Home Page', () => {
+    it('successfully loads', () => {
+      cy.visit('http://127.0.0.1:5173/') 
+    })
+  })
+
+  
+  
+  
