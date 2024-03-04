7c2d43d214f287be6e9c2a6e655fa8aa8ac24ed4
vegardvasset <vva034@student.uib.no>
2024-03-04 17:44:34 +0100

    Removed branch from yml file

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
-const connectDB = require('./db'); // Updated to require the connectDB function
+const connectDB = require('./db'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
